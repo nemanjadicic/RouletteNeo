85cb56a6ccf56352d71a32057d55922f2cea043d
Nemanja Dicic <nemanja.dicic@gmail.com>
2019-10-22 10:42:40 +0200

    Wire Up BuySubForm
    Implements BuySubForm functionalities and adds DiealogResult ShowDialog() to IRouletteSession interface

--- RouletteNeoLibrary/IRouletteSession.cs
+++ RouletteNeoLibrary/IRouletteSession.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RouletteNeoLibrary
 {
@@ -42,6 +43,7 @@ namespace RouletteNeoLibrary
 
 
 
+        DialogResult ShowDialog();
         void Close();
     }
 }
--- RouletteNeoUI/Forms/BuySubForm.cs
+++ RouletteNeoUI/Forms/BuySubForm.cs
@@ -1,4 +1,5 @@
 ﻿using RouletteNeoLibrary;
+using RouletteNeoLibrary.DataAccess;
 using RouletteNeoLibrary.Models;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,42 @@ namespace RouletteNeoUI.Forms
 {
     public partial class BuySubForm : Form
     {
-        public BuySubForm(UserModel user, IRouletteSession rouletteSessionForm)
+        UserModel currentUser = new UserModel();
+
+        IRouletteSession previousRouletteSession;
+        
+        public BuySubForm(UserModel user, IRouletteSession rouletteSessFrm)
         {
             InitializeComponent();
+
+            currentUser = user;
+
+            previousRouletteSession = rouletteSessFrm;
+
+            Button[] allButtons = { oneMonthButton, sixMonthButton, yearButton };
+
+            foreach (Button button in allButtons)
+            {
+                button.Click += Button_Click;
+            }
+        }
+
+        private void BuySubAndOpenRouletteNeo(UserModel user, IRouletteSession rouletteSessFrm)
+        {
+            user.BoughtSub = true;
+
+            SQLiteDataAccess.UpdateUserSubscription(currentUser);
+
+            rouletteSessFrm = new RouletteSessionForm(user);
+            this.Hide();
+            previousRouletteSession.Close();
+            rouletteSessFrm.ShowDialog();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            BuySubAndOpenRouletteNeo(currentUser, previousRouletteSession);
         }
     }
 }
